abcb493e870948fb687649e63f824b5dc5af7c24
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-04-28 19:20:51 +0100

    Removed unused files

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:networkingdemo/screens/album_photo/a_p.dart';
 import 'package:networkingdemo/screens/home.dart';
 import 'package:networkingdemo/screens/post_comment/p_c.dart';
 import 'package:networkingdemo/screens/user_album/u_a.dart';
-import 'package:networkingdemo/screens/u_a_p.dart';
-import 'package:networkingdemo/screens/u_p_c.dart';
 import 'package:networkingdemo/screens/user_todo/u_t.dart';
 
 void main() {
@@ -22,8 +20,6 @@ class MyApp extends StatelessWidget {
         '/pc': (context) => PCScreen(),
         '/ut': (context) => UTScreen(),
         '/ua': (context) => UAScreen(),
-        '/uap': (context) => UAPScreen(),
-        '/upc': (context) => UPCScreen(),
       },
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
--- lib/screens/u_a_p.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class UAPScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
